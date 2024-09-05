3f76c61cacdbeb194d152f9a9750b957b9b36057
tracy <keeyatracy@gmail.com>
2024-09-05 11:24:03 +0300

    update

--- todo/views.py
+++ todo/views.py
@@ -24,18 +24,18 @@ def add_family_member(request):
 
 
 def task_list(request):
-    tasks = Task.objects.all()
-    return render(request, 'todo/task_list.html', {'tasks': tasks})
+    task = Task.objects.all()
+    return render(request, 'todo/task_list.html', {'tasks': task})
 
 def add_task(request):
     if request.method == 'POST':
-        form = TaskForm(request.POST)
-        if form.is_valid():
-            form.save()
+        tasks = TaskForm(request.POST)
+        if tasks.is_valid():
+            tasks.save()
             return redirect('task_list')
     else:
-        form = TaskForm()
-    return render(request, 'todo/add_task.html', {'form': form})
+        tasks = TaskForm()
+    return render(request, 'add_task.html', {'tasks': tasks})
 
 """def edit_task(request, task_id):
     task = get_object_or_404(Task, id=task_id)
@@ -56,14 +56,14 @@ def delete_task(request, task_id):
 
 def workdone(request):
     workdone = WorkDone.objects.all()
-    return render(request, 'todo/workdone_list.html', {'workdone': workdone})
+    return render(request, 'todo/work_done.html', {'workdone': workdone})
 
 def add_workdone(request):
     if request.method == 'POST':
         form = WorkDoneForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('workdone_list')
+            return redirect('work_done')
     else:
         form = WorkDoneForm()
-    return render(request, 'todo/add_workdone.html', {'form': form})
+    return render(request, 'todo/add_work_done.html', {'form': form})
